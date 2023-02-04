c9c4be144228ceafa60cf079db969bff5ad6617e
Chachavez-slide <chavesj1899@gmail.com>
2023-02-04 10:14:26 -0500

    2/4 Object Exercise plans
    Algorithm plan

--- labs/lab4/js/lab4.js
+++ labs/lab4/js/lab4.js
@@ -3,9 +3,10 @@
 //----------------------------1 - Data-driven display w/ function-----------------------------------//
 
 //Make an object with all the properties for a graphic on the screen
-// - width, height, color, position. These numbers could represent positions of wheels,
-// eye colors, or rainbow lengths. In your create or draw function draw that object to the screen using the 
-//data in your object - using a series of fill commands, draw commands, and whatever else you need to draw the object to the screen.
+// - width, height, color, position. 
+//These numbers could represent positions of wheels ,eye colors, or rainbow lengths.
+// In your create or draw function draw that object to the screen using the data in your object - 
+//using a series of fill commands, draw commands, and whatever else you need to draw the object to the screen.
 // Try to not hard-code any numbers in the drawing commands - only use the object properties to set sizes, colors, or position.
 
 //The, create a function that takes one argument and draws an object to the screen using that object argument. 
@@ -17,7 +18,23 @@
 
 //---plan 1--//
 
+//object
 
+//let objectNamePlaceholder = {
+  //object width
+  //object height
+  //object color
+  //object position
+//}
+
+
+//function setup(){
+  //createCanvas()
+//}
+
+//draw function
+    //object is the parameter.
+//
 
 
 
@@ -43,6 +60,34 @@
 
 //---plan 2--//
 
+//object 1
+//let rectangleA = {
+//object x
+//object y  
+//object width
+  
+//}
+
+
+//object 2
+//let rectangleB = {
+  //object x
+  //object y
+//object width
+
+//}
+
+//function draw(){
+
+// If up key (UP_ARROW) is pressed = rectangleA goes up, rectangleB goes down.
+
+//run a function with the parameter (up_arrow)?
+//rectangleA.x //rectangleA.y + unknown y value
+//rectangleB.x //rectangleB.y - unknown y value
+
+//If down key (DOWN_ARROW) is pressed= rectangleA goes down, rectangleB goes up.
+
+//}
 
 
 
@@ -53,5 +98,12 @@
 //should be used in your update function.
 
 
+//turn ball into object
+//object properties
+    //object width
+    //object height
+    //object color
+    //object speed    // would this be an equation?
+//insert ball bounce assignment code here.
 
 //---plan 3--//
