e163dea4542f6a4139b6986b7ad8c6ac8ee4b780
Chachavez-slide <chavesj1899@gmail.com>
2023-04-10 20:46:12 -0400

    4/10 Event 1 notes

---
+++ labs/lab10/js/lab10_1.js
@@ -0,0 +1,25 @@
+/*
+Create an application with three grey, square divs, in a row. 
+They should be 200px by 200px, and all floated left. Give them a margin of 5px.
+
+Using only one event handler, write event listeners to respond to a click on each element.
+ Each element should change to a different color: one red, one green, and one blue.
+  Use a data attribute on the elements to store the color to be changed to.
+
+The result, after clicking on the first and third, should look something like this:
+*/
+
+//plans 
+
+//html
+
+//create 3 divs 
+//classes: div1, div2, div3
+// set up classes to have even length sides 
+
+//java use an even listener onClick.
+//when clicked change div 1 to #ff0000
+//div 2 to #00ff00
+//div 3 to #0000ff
+
+// change each class atttribute when div clicked.
---
+++ labs/lab10/js/lab10_3.js
@@ -0,0 +1,22 @@
+/*
+Flash Cards
+
+An application with 3 buttons that ask questions. 
+For instance, they might ask "What is the capital of Indiana?".
+ Each button should have a data-answer attribute that lists the answer.
+When a button is clicked, display the answer to the button's question in a div.
+ Use only one function, and that function must make use of the button's data-attribute.
+*/
+
+//plans
+
+//set up 3 buttons
+
+//What is Indiana's capitol
+//Indianapolis
+
+//what is illinois's captial
+//Springfield
+
+//what is Michigan's captial?
+//Lansing
